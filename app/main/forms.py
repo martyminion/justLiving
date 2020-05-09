d064df5971b5f897d5e51c6bd63b12af930fd137
martyminion <martkimwaweru@gmail.com>
2020-05-09 17:00:58 +0300

    adds blog and comment forms as well as addblog view

---
+++ app/main/forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField,TextAreaField
+from wtforms.validators import email,Required,Email,EqualTo
+from ..models import Blog,Comment
+from wtforms import ValidationError
+
+class BlogForm(FlaskForm):
+  '''
+  defines the fields to be filled when writing a blog
+  '''
+  title = StringField("Title",validators=[Required()])
+  category = StringField("Category",validators=[Required()])
+  body = TextAreaField("Body")
+  submit = SubmitField("Add")
+
+class CommentForm(FlaskForm):
+  '''
+  defines the fields to be filled for a comment
+  '''
+  body = TextAreaField('Always be Kind')
+  SubmitField = SubmitField('comment')
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
 from flask import render_template,redirect,request,abort
 from . import main
 from flask_login import login_required
+from ..models import Blog,Comment
+from .. import db
+from .forms import BlogForm,CommentForm
 
 @main.route('/')
 def index():
@@ -11,4 +14,18 @@ def index():
   title = "Homepage"
 
   return render_template('index.html',title  = title)
+
+@main.route('/new/blog')
+def add_blog():
+  '''
+  adds a new blog
+  '''
+  blog_form = BlogForm()
+
+  if blog_form.validate_on_submit():
+    new_blog = Blog(title = blog_form.title.data, category = blog_form.category.data, blog_body = blog_form.body.data)
+    new_blog.save_blog()
+    return redirect('blogs.html')
+  title = "New Blog"
+  return render_template('newblog.html',blogform = blog_form, title = title)
 
